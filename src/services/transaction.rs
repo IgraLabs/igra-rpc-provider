e5b8276544a64d8404d6c307f6c7195d84411642
Mike Zak <feanorr@gmail.com>
2025-03-30 15:16:37 +0300

    Use complete payload

--- src/services/transaction.rs
+++ src/services/transaction.rs
@@ -91,7 +91,7 @@ fn prepare_payload(tx_bytes: &[u8]) -> Result<Vec<u8>, Box<dyn Error + Sync + Se
 
     payload_buffer.extend_from_slice(&[0x97, 0xB1]);
     payload_buffer.extend_from_slice(&[0xA2]);
-    payload_buffer.extend_from_slice(&tx_bytes);
+    payload_buffer.extend_from_slice(&zipped_payload);
 
     Ok(payload_buffer.to_vec())
 }
