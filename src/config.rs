ac688b79144b684ab0a7873f3cc1daa9d1d28b05
Vadim Konstantinov <vadim.konstantinov@gmail.com>
2025-04-20 19:54:53 +0300

    fix: Enable env variables overrides default config values
    Previously, the app config ignored env variables, making it impossible to dynamically override default values from `config.toml`. This issue impacts dockerized deployments, where dynamic configuration using env variables is essential.
    
    Although the `config` crate is assumed to support this behavior automatically, it did not work as expected.
    
    To address this, until the expected behavior of the `config` is assured, introduced manual mapping of environment variables to configuration keys, allowing them to properly override defaults.
    
    Note, anticipating adjustment of the key `wallet.wallet_daemon_uri` to `wallet.daemon_uri` to make keys more user-friendly in further commits, this key is explicitly mapped to the `WALLET_DAEMON_URI` env variable instead of `WALLET_WALLET_DAEMON_URI`.

--- src/config.rs
+++ src/config.rs
@@ -38,14 +38,31 @@ fn default_enable_whitelist() -> bool {
 
 impl AppConfig {
     pub fn load() -> Self {
-        let settings = Config::builder()
+        let mut settings = Config::builder()
             .add_source(File::with_name("config"))
             .add_source(
-                Environment::default().separator("_"), // for nested keys
-            )
-            .build()
-            .unwrap();
+                Environment::default()
+                    .separator("_") // Use "_" to allow nested structures
+                    .prefix(""),    // Ensure there is no optional prefix
+            );
 
-        settings.try_deserialize().expect("Invalid config format")
+        // @todo Make env vars to overwrite defaults w/o this manual mapping.
+        for (key, value) in std::env::vars() {
+            let mapped_key = match key.as_str() {
+                "SERVER_HOST" => "server.host",
+                "SERVER_PORT" => "server.port",
+                "EL_URL" => "el.url",
+                "WALLET_DAEMON_URI" => "wallet.wallet_daemon_uri",
+                "WALLET_TO_ADDRESS" => "wallet.to_address",
+                "SECURITY_ENABLE_WHITELIST" => "security.enable_whitelist",
+                _ => continue, // Skip irrelevant
+            };
+            settings = settings.set_override(mapped_key, value).unwrap();
+        }
+
+        settings
+            .build()
+            .unwrap()
+            .try_deserialize().expect("Invalid config format")
     }
 }
